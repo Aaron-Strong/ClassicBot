7bb3f9669e4a9b1475ff6295d5210a0319e2d488
Aaron <aaron12-13@hotmail.co.uk>
2017-06-29 00:08:21 +0100

    int

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const {token} = require('./settings')
+const { token } = require("./settings");
 client.login(token);
 
-client.on('message', message => {
+client.on("message", message => {
   // Voice only works in guilds, if the message does not come from a guild,
   // we ignore it
   if (!message.guild) return;
 
-  if (message.content === '!classic') {
+  if (message.content === "!classic") {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voiceChannel) {
-        const voiceChannel = message.member.voiceChannel
-        voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('Classic!');
-          const dispatcher = connection.playFile('./classic.mp3')
-          .on('end', () => {
-            voiceChannel.leave()
-          });
+      const voiceChannel = message.member.voiceChannel;
+      voiceChannel
+        .join()
+        .then(connection => {
+          // Connection is an instance of VoiceConnection
+          message.reply("Classic!");
 
-          dispatcher.on('error', e => {
-              // Catch any errors that may arise
-              console.log(e);
+          const dispatcher = connection
+            .playFile("./classic.mp3")
+            .on("end", () => {
+              voiceChannel.leave();
+            });
+
+          dispatcher.on("error", e => {
+            // Catch any errors that may arise
+            console.log(e);
           });
-          
         })
         .catch(console.log);
     } else {
-      message.reply('Twat join a fucking voice channel');
+      message.reply("Twat join a fucking voice channel");
+    }
+  }
+
+  if (message.content === "!int") {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      const voiceChannel = message.member.voiceChannel;
+      voiceChannel
+        .join()
+        .then(connection => {
+          // Connection is an instance of VoiceConnection
+          message.reply("INT INT INT INT INT INT");
+          // const dispatcher = connection.playFile('./classic.mp3')
+
+          const dispatcher = connection.playFile("./int.mp3").on("end", () => {
+            voiceChannel.leave();
+          });
+
+          dispatcher.on("error", e => {
+            // Catch any errors that may arise
+            console.log(e);
+          });
+        })
+        .catch(console.log);
+    } else {
+      message.reply("Twat join a fucking voice channel");
     }
   }
 });
